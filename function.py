2276139cd0ed8f35faba81091abc440f8dd79f97
liuyuxiu <fighting6198@qq.com>
2026-01-04 08:21:46 +0800

    在train_sam添加logger

--- function.py
+++ function.py
@@ -191,8 +191,7 @@ def train_sam(
  
             loss1 = lossfunc(pred, masks)
             loss2 = lossfunc(mask_prompts ,masks)
-            print('mask prompt和gt的损失值：', loss2.item())
-            print('pre和gt的损失值：', loss1.item())
+            logger.info(f'mask prompt和gt的损失值：{loss2.item()}, pre和gt的损失值：{loss1.item()}')
             loss= 0.1*loss1+0.9*loss2
             epoch_loss += loss.item()
 
